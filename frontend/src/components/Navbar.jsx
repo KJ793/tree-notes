552d5d555e8478bb5897471c799d9bb84cf1276b
Johannes Olsen <johannes.j.olsen@gmail.com>
2026-08-01 09:36:35 +1000

    updated with new frontend stuff

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>TreeNotes</h2>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <h2>TreeNotes</h2>
+
+      <button>Dashboard</button>
+      <button>Notes</button>
+      <button>Graph</button>
+      <button>Search</button>
+      <button>Tags</button>
+      <button>Settings</button>
+    </aside>
+  );
+}
+
+export default Sidebar;
