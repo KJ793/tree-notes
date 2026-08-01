eea1e33442004e48b26be0b1b2d66495d59a3196
Jagpreet Singh <jagpreet.uow@gmail.com>
2026-08-02 01:54:12 +1000

    changed dashboard layout to match figma

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -1,6 +1,5 @@
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
-import DashboardHeader from "./components/DashboardHeader";
 import DashboardContent from "./components/DashboardContent";
 
 function Dashboard() {
@@ -12,10 +11,6 @@ function Dashboard() {
         <Sidebar />
 
         <section className="dashboard-main">
-          <DashboardHeader />
-
-          <p className="dashboard-welcome">Welcome back!</p>
-
           <DashboardContent />
         </section>
       </div>
